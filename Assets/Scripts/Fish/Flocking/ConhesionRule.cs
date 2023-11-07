0190251d58b46fed0079b2bd1dbd227d045259d8
ChoiGyuSong <cgs15002@gmail.com>
2023-11-07 12:31:11 +0900

    ㅇㅇ

---
+++ Assets/Scripts/Fish/Flocking/ConhesionRule.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class CohesionRule
+{
+    public Vector3 GetDirection(Transform agent, List<Transform> neighbor)
+    {
+        if (agent == null || neighbor == null || neighbor.Count == 0)
+            return Vector3.zero;
+
+        Vector3 averagePos = Vector3.zero;
+
+        foreach (var ne in neighbor)
+        {
+            averagePos += ne.position;
+        }
+
+        averagePos /= neighbor.Count;
+
+        return (averagePos - agent.transform.position).normalized;
+    }
+}
